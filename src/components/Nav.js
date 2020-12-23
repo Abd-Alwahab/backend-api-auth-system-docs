1191d3a0717c800c5a36cd339272e8ecd0818504
abdalwahab <bayanadam10@gmail.com>
2020-12-23 16:27:32 +0300

    working on the install page

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,34 +3,37 @@ import { Link } from "react-router-dom";
 
 export default function Nav() {
   return (
-    <div className="header__nav">
-      <div className="nav__content">
-        <nav className="nav">
-          <h1 className="logo">AbdThy</h1>
-          <ul className="list">
-            <li className="item">
-              <Link className="link" to="/home">
-                Home
-              </Link>
-            </li>
+    <>
+      <div className="header__nav">
+        <div className="nav__content">
+          <nav className="nav">
+            <h1 className="logo">AbdThy</h1>
+            <ul className="list">
+              <li className="item">
+                <Link className="link" to="/home">
+                  Home
+                </Link>
+              </li>
 
-            <li className="item">
-              <Link className="link" to="/docs">
-                Docs
-              </Link>
-            </li>
-            <li className="item">
-              <Link className="link" to="/about">
-                About
-              </Link>
-            </li>
+              <li className="item">
+                <Link className="link" to="/docs">
+                  Docs
+                </Link>
+              </li>
+              <li className="item">
+                <Link className="link" to="/about">
+                  About
+                </Link>
+              </li>
 
-            <li className="item">
-              <Link className="link">Developer</Link>
-            </li>
-          </ul>
-        </nav>
+              <li className="item">
+                <Link className="link">Developer</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
-    </div>
+      <div className="nav_clear"></div>
+    </>
   );
 }
