dd8dd4d2104b6e7886e560a7b459aac1167eb3c0
abdalwahab <bayanadam10@gmail.com>
2020-12-23 11:31:03 +0300

    working on the install page

--- src/components/Installation.js
+++ src/components/Installation.js
@@ -1,4 +1,6 @@
 import React from "react";
+import step_1 from "./../media/installation/step_1.png";
+import step_2 from "./../media/installation/step_2.png";
 
 function Installation() {
   return (
@@ -10,6 +12,19 @@ function Installation() {
         <div className="hilight">
           git clone https://github.com/Abd-Alwahab/auth-system.git
         </div>
+        <p className="docs_desc">
+          Now, you have the project localy on your OS, its time for you to open it in your
+          code editor (I use VSCode)
+          <img src={step_1} alt="open folder" className="docs__img" />
+        </p>
+        <p className="docs_desc">
+          Now, once you open the project in your code editor, we need to run the following
+          command to install some npm packages that we need, so open your terminal in your
+          code editor and run the following command
+          <div className="hilight">npm install</div>
+          <p className="docs_desc">That will ahead and add the node_modules </p>
+          <img src={step_2} alt="open folder" className="docs__img" />
+        </p>
       </p>
     </div>
   );
