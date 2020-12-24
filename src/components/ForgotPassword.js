2787b619bb5ba57a5752b8c8f4d1f7624f8d526a
abdalwahab <bayanadam10@gmail.com>
2020-12-24 19:40:06 +0300

    working on the forgot password page

---
+++ src/components/ForgotPassword.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ForgotPassword() {
+  return (
+    <div>
+      <h2>How To Reset a User Password </h2>
+    </div>
+  );
+}
+
+export default ForgotPassword;
--- src/pages/Docs.js
+++ src/pages/Docs.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import Container from "../components/Container";
+import ForgotPassword from "../components/ForgotPassword";
 import Installation from "../components/Installation";
 import Login from "../components/Login";
 import Signup from "../components/Signup";
@@ -15,6 +16,7 @@ export default function Docs({ match }) {
         <Route path={`${match.path}/installation`} component={Installation} />
         <Route path={`${match.path}/signup`} component={Signup} />
         <Route path={`${match.path}/login`} component={Login} />
+        <Route path={`${match.path}/forgotpassword`} component={ForgotPassword} />
       </div>
     </div>
   );
