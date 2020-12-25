1cb261ff4f30c8f8a60ff910d28c275eba5e8317
abdalwahab <bayanadam10@gmail.com>
2020-12-25 21:03:11 +0300

    working on the update my password

--- src/components/updatePassword.js
+++ src/components/updatePassword.js
@@ -4,6 +4,46 @@ function updatePassword() {
   return (
     <div>
       <h2>Update My Password</h2>
+
+      <p className="docs_desc">
+        To Update the password for the currently logged in user, you need to make a PATCH
+        request to the following endpoint
+      </p>
+      <br />
+      <span className="hilight m-m">
+        PATCH <div className="empty_space"></div>{" "}
+        http://localhost:3000/api/v1/users/updateMyPassword
+      </span>
+
+      <p className="docs_desc m-m">
+        This will make a patch request to the api and it will try find the user and update
+        his/her password, however, In order for this to work then we need to provide some
+        needed info to veryfiy this proccess, IN our case we need a valid json web token
+        to be sent as an http header and it shouls has the name " token ", and we need to
+        send three paeaces of information in the request body which are currentPassword,
+        newPassword, passwordConfirm as shown in the tabel below:
+      </p>
+
+      <table>
+        <tr>
+          <th>Property</th>
+          <th>Required</th>
+        </tr>
+
+        <tr>
+          <td>currentPassword</td>
+          <td>True</td>
+        </tr>
+        <tr>
+          <td>newPassword</td>
+          <td>True</td>
+        </tr>
+
+        <tr>
+          <td>passwordConfirm</td>
+          <td>True</td>
+        </tr>
+      </table>
     </div>
   );
 }
