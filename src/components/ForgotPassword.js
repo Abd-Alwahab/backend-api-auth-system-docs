c8fef99f0496e774c697ba4eee01276de0a826c8
abdalwahab <bayanadam10@gmail.com>
2020-12-25 20:47:45 +0300

    finish the forgot password page

--- src/components/ForgotPassword.js
+++ src/components/ForgotPassword.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 import step_1 from "./../media/forgotpassword/step_1.png";
+import step_2 from "./../media/forgotpassword/step_2.png";
+import step_3 from "./../media/forgotpassword/step_3.png";
+import step_4 from "./../media/forgotpassword/step_4.png";
 
 function ForgotPassword() {
   return (
@@ -40,6 +43,74 @@ function ForgotPassword() {
       <p className="docs_desc m-s">
         This will try to check for the provided info and if everything is corect then it
         will send a passwrod reset token to the user email.
+      </p>
+
+      <img src={step_2} alt="forgot password step" className="docs__img" />
+
+      <p className="docs_desc m-s">
+        The user should get an email in his/her email that has the reset password url and
+        they can click on it and that should redirect the user to a password reset page
+      </p>
+
+      <p className="docs_desc m-s">
+        In the password reset page, we need to make a post request to the following
+        endpoint
+      </p>
+      <br />
+      <span className="hilight m-m">
+        POST <div className="empty_space"></div>{" "}
+        http://localhost:3000/api/v1/users/resetPassword/:token
+      </span>
+
+      <p className="docs_desc m-m">
+        however, In order for this thing to work as it should then we need to send along
+        the password reset token that the user recived in their email as a query parameter
+        so the endpoint will look like this
+      </p>
+
+      <br />
+      <span className="hilight m-m">
+        POST <div className="empty_space"></div>{" "}
+        http://localhost:3000/api/v1/users/resetPassword/
+        <span className="danger">dfklahdlkjshzjksfhsksfkjsfskf</span>
+      </span>
+
+      <br />
+      <p className="docs_desc m-m">
+        so the text that you can see in the red color is the password reset token and it
+        has to be passed as a query parameter, along the token you need to send two more
+        information in the request body to the api
+      </p>
+
+      <img src={step_3} alt="forgot password step" className="docs__img" />
+
+      <table>
+        <tr>
+          <th>Property</th>
+          <th>Required</th>
+        </tr>
+
+        <tr>
+          <td>password</td>
+          <td>True</td>
+        </tr>
+
+        <tr>
+          <td>passwordConfirm</td>
+          <td>True</td>
+        </tr>
+      </table>
+
+      <p className="docs_desc m-s">
+        If you followed the mentioned steps then you should get a response that look
+        something like this:
+      </p>
+
+      <img src={step_4} alt="forgot password step" className="docs__img" />
+
+      <p className="docs_desc m-s">
+        If so then that confirmed that the password for the user has been reseted and new
+        json web token was created to log the user in.
       </p>
     </div>
   );
