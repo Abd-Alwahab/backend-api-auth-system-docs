d1a3d4270db76c50fa9bb633e0019b2dbb22324f
abdalwahab <bayanadam10@gmail.com>
2020-12-26 17:09:47 +0300

    working on the get all users page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,15 @@
 import React from "react";
 
 export default function About() {
-  return <div>About</div>;
+  return (
+    <div className="about">
+      <h2>About This Project</h2>
+      <p className="dark">
+        This is an open source project, anything in this project is free for anyone who
+        want to use or to contibrute to, it built by abd alwahab who is a full stack
+        developer, this is project is build to help beginners developer to build real
+        world projects base on real backend
+      </p>
+    </div>
+  );
 }
