684df7aff085f214749729526c4e2d8a340ea723
abdalwahab <bayanadam10@gmail.com>
2020-12-26 16:46:07 +0300

    working on the get all users page

--- src/components/GetAllUsers.js
+++ src/components/GetAllUsers.js
@@ -1,11 +1,25 @@
 import React from "react";
 import Admin from "./Admin";
+import Authrization from "./authrization";
+import step_1 from "./../media/getAllUsers/step_1.png";
 
 function GetAllUsers() {
   return (
     <div>
       <h2>Get All Users</h2>
       <Admin />
+      <Authrization />
+      <p className="docs_desc">
+        To get all the users in our database, you need to make a get request to the
+        following endpoint
+      </p>
+      <br />
+
+      <span className="hilight m-m">
+        GET <div className="empty_space"></div> http://localhost:3000/api/v1/users/
+      </span>
+
+      <img src={step_1} alt="forgot password step" className="docs__img" />
     </div>
   );
 }
