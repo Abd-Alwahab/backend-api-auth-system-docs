4016dceb5df8970d70b2db5cd3d513e7319abe4f
abdalwahab <bayanadam10@gmail.com>
2020-12-29 22:27:46 +0300

    working on click to copy

--- src/components/HighLightText.js
+++ src/components/HighLightText.js
@@ -1,34 +1,24 @@
 import React, { useState } from "react";
 import copy from "copy-to-clipboard";
+import { ToastContainer, toast } from "react-toastify";
 
 function HighLightText({ text, endpoint }) {
   const [isCopied, setIsCopied] = useState(false);
+
+  const notify = () => toast("Text Copied");
   return (
     <div
       onClick={() => {
         copy(endpoint);
-        setIsCopied(true);
+        notify();
       }}
       className="textCopied-parent"
     >
-      {isCopied ? (
-        <div className={isCopied ? "textCopied shown" : "textCopied hidden"}>
-          Text Copied{" "}
-          <div
-            className="close"
-            onClick={() => {
-              setIsCopied(false);
-              console.log("Close");
-            }}
-          >
-            {" "}
-            x{" "}
-          </div>
-        </div>
-      ) : null}
       <span className="hilight">
         {text} <div className="empty_space"></div> {endpoint}
       </span>
+
+      <ToastContainer />
     </div>
   );
 }
